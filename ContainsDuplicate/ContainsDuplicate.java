1d54b524c1a24c0d163eaf1e7a2be121811847ac
MaksimLiu <maksimliu06@gmail.com>
2016-03-06 09:08:33 +0000

    First Commit

---
+++ ContainsDuplicate/ContainsDuplicate.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        Set set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
