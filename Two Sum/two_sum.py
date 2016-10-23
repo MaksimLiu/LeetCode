fd03d921b823cbf1956667aa8c6a58be4bf4a4ac
MaksimLiu <maksimliu06@gmail.com>
2016-10-23 16:24:38 +0800

    init

---
+++ Two Sum/two_sum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        length = len(nums)
+
+        for i, i_value in enumerate(nums):
+
+            j = i + 1
+            while (j < length):
+                if (i_value + nums[j]==target):
+                    return [i, j]
+
+                j += 1
